8bf5cb5cb5c5008794d6b1f4b3cfa56867450f9a
alichherawalla <mohammed.ali.chherawalla@gmail.com>
2026-09-26 18:35:53 +0530

    fix(kev): avoid full-precision CUDA adapter merge allocation

--- resources/bin/kev-local-server.py
+++ resources/bin/kev-local-server.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4ae7ace3e366b1c7eac241212de09eaa6d3a749de628d9dbf80471e54b8611e
-size 1984
+oid sha256:d8bc3c78b975ca8c6b97b19a074fe3a1eb68c4fc508ff6bd42744043383968b8
+size 2433
